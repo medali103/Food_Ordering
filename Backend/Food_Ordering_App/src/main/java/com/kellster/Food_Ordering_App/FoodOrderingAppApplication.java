db540d1821c45cb3ee148c0780c3ff36d8e3bc97
mokellat <kellatis55@gmail.com>
2024-03-16 21:56:46 +0000

    initializing the project

---
+++ Backend/Food_Ordering_App/src/main/java/com/kellster/Food_Ordering_App/FoodOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package com.kellster.Food_Ordering_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAppApplication.class, args);
+	}
+
+}
---
+++ Backend/Food_Ordering_App/src/test/java/com/kellster/Food_Ordering_App/FoodOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellster.Food_Ordering_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
